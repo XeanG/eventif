efe874b2c1c7c359f1b223bf79316e091042a43b
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-07-07 14:18:45 +0000

    testando o sistema

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 
 class testHome(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
     def test_home(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template_used(self):
+        self.assertTemplateUsed(self.response, 'index.html')
