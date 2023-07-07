e398c072a13ccda3290d46dbab2b26591aa63ba6
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-07-07 14:13:40 +0000

    a

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class testHome(TestCase):
+    def test_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
