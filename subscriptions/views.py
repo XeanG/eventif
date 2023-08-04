f1e3a1834f10564de7d0e85d2bcb6463d4245dcb
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-08-04 15:00:50 +0000

    lezgooo

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,30 +1,14 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render 
 from subscriptions.forms import SubscriptionForm
+from django.core import mail
+from django.template.loader import render_to_string 
 
 def subscribe(request):
    if request.method == 'POST':
-      mail.send_mail('Confirmação de Inscrição',MESSAGE,'contato@eventif.com.br',['contato@eventif.com.br', 'jeanpr667@gmail.com'])
+      body = render_to_string('subscriptions/subscription_email.txt')
+      mail.send_mail('Confirmação de Inscrição',body,'contato@eventif.com.br',['contato@eventif.com.br', 'jeanpr667@gmail.com'])
       return HttpResponseRedirect('/inscricao/')
    context = {"form":SubscriptionForm()}
    return render(request, 'subscriptions/subscription_form.html', context)
 
-MESSAGE = """
-Olá! Tudo bem?
-
-Muito obrigado por se inscrever no Eventif.
-
-Estes foram os dados que vocẽ enviou na sua
-inscrição.
-
-Nome: Jean Pierre
-CPF: 12345678901
-Email: jeanpr667@gmail.com
-Telefone: 53 991561217
-
-Em até 48h úteis alguem da nossa equipe entrará em contato com você para concluirmos a sua
-inscrição.
-
-Atenciosamente,
-Equipe EventIF
-"""
