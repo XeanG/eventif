a9d8be04b307ec5fbf2d351fd89b61cfbbae6ebc
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-08-04 12:50:02 +0000

    uoo maga

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def subscribe(request):
-   return render(request, 'subscripitions/subscripition_form.html')
+   return render(request, 'subscriptions/subscription_form.html')
