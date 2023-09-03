c6cfe904dd238f6f8928c0750aef27a380743d38
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-09-03 16:53:29 -0300

    Update tests.py
    dando aquele update

--- core/tests.py
+++ core/tests.py
@@ -9,3 +9,6 @@ class testHome(TestCase):
 
     def test_template_used(self):
         self.assertTemplateUsed(self.response, 'index.html')
+        
+    def test_subscription_link(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
