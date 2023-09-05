086bd6f831473d0d8d5be5b578d4f52b4f04767c
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-09-05 20:45:27 -0300

    vamos embora amigos

--- eventif/urls.py
+++ eventif/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import home
-from subscripitons.views import subscribe
+from subscriptions.views import subscribe
 
 urlpatterns = [
     path('', home),
