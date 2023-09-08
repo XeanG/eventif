c14110216b622284e728c4723716f9ce4f3b7e4d
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-09-08 15:32:33 +0000

    aaab

--- eventif/urls.py
+++ eventif/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from core.views import home
 from subscriptions.views import subscribe
+from contact.views import contact_view 
 
 urlpatterns = [
     path('', home),
     path('inscricao/', subscribe),
     path("admin/", admin.site.urls),
+    path('contact/', contact_view, name='contact_view')
 ]
