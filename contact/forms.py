5a4c9d7ecd09e3838090c5c2d442531ba82bd6ac
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-09-08 15:58:00 +0000

    errei fui mlk

--- contact/forms.py
+++ contact/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    nome = forms.CharField(label='name')
-    telefone = forms.CharField(label='phone', required=False) 
+    name = forms.CharField(label='Nome')
+    phone = forms.CharField(label='Telefone', required=False) 
     email = forms.EmailField(label='Email')
-    mensagem = forms.CharField(widget=forms.Textarea)
+    message = forms.CharField(widget=forms.Textarea)
