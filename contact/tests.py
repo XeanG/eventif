a8d7f52bdf855d35e13db63cb81719bc2a1ab6b0
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-09-08 14:36:20 +0000

    coringando.png

--- contact/tests.py
+++ contact/tests.py
@@ -2,16 +2,17 @@ from django.core import mail
 from django.urls import reverse
 from .forms import ContactForm
 from django.test import TestCase
+from .views import visualizar_contato
 
 class TestesContato(TestCase):
     def setUp(self):
         self.dados_validos = {
             'nome': 'Whesley Souza', 'email': 'whesley.souza@aluno.riogrande.ifrs.edu.br', 'mensagem': 'TESTANDO'
         }
-        self.response = self.client.post(reverse('contact_view'), data=self.dados_validos)
+        self.response = self.client.post(reverse('visualizar_contato'), data=self.dados_validos)
 
     def test_get(self):
-        response = self.client.get(reverse('contact_view'))
+        response = self.client.get(reverse('visualizar_contato'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contact/contact_form.html')
 
@@ -22,7 +23,7 @@ class TestesContato(TestCase):
         dados_invalidos = {
             'nome': '', 'email': 'invalid-email', 'mensagem': 'invalido'
         }
-        response_invalida = self.client.post(reverse('contact_view'), data=dados_invalidos)
+        response_invalida = self.client.post(reverse('visualizar_contato'), data=dados_invalidos)
         self.assertEqual(response_invalida.status_code, 200)
 
     def test_enviar_email(self):
--- contact/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Outras URLs aqui
-    path('contato/', views.contact_view, name='contact_view'),
-]
