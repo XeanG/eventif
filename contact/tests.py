a78da253d199225b1ef512707b10fd1d5ff0d300
Jean Pierre de Oliveira Teixeira <61829569+XeanG@users.noreply.github.com>
2023-09-08 15:05:08 +0000

    ?

--- contact/tests.py
+++ contact/tests.py
@@ -1,30 +1,31 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.core import mail
 from .forms import ContactForm
-from .views import contact_view 
-
 
 class TestesContato(TestCase):
+    def setUp(self):
+        self.dados_validos = {
+            'nome': 'Whesley Souza', 'email': 'whesley.souza@aluno.riogrande.ifrs.edu.br',  'mensagem': 'TESTANDO'
+        }
+        self.response = self.client.post(reverse('contact_view'), data=self.dados_validos)
+
     def test_get(self):
         response = self.client.get(reverse('contact_view'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contact/contact_form.html')
 
     def test_post_valido(self):
-        dados_validos = {
-            'nome': 'Exemplo', 'email': 'exemplo@email.com', 'mensagem': 'Mensagem de teste'
-        }
-        response = self.client.post(reverse('contact_view'), data=dados_validos)
-        self.assertEqual(response.status_code, 302)  # Redirecionamento após o envio
+        self.assertEqual(self.response.status_code, 302)
 
     def test_post_invalido(self):
         dados_invalidos = {
-            'nome': '', 'email': 'invalid-email', 'mensagem': ''
+            'nome': '', 'email': 'invalid-email', 'mensagem': 'invalido'
         }
         response = self.client.post(reverse('contact_view'), data=dados_invalidos)
-        self.assertEqual(response.status_code, 200)  # Página de formulário deve ser recarregada
+        self.assertEqual(response.status_code, 200)
 
     def test_enviar_email(self):
-        # Simular o envio de um email aqui e verificar se ele foi enviado corretamente
-        # Este teste pode ser mais complexo e envolver um mock para enviar email.
-        pass
+        self.assertEqual(self.response.status_code, 302)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'contato@eventif.com.br')
